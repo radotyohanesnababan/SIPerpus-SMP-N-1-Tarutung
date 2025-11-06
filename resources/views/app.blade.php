50fd0dafdbfa97b150d930cdac85f146e771d7ce
radotyohanesnababan <radot2510@gmail.com>
2025-11-06 11:42:20 +0000

    welcome edit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,9 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="SPARTA - Sistem Perpustakaan SMP Negeri 1 Tarutung. Koleksi buku lengkap, kemudahan akses, dan layanan terbaik untuk mendukung pembelajaran Anda.">
+        <meta name="keywords" content="Perpustakaan, SMP Negeri 1 Tarutung, Buku, Koleksi Buku, Layanan Perpustakaan, Pendidikan, Sumber Belajar, SPARTA, Buku Online, Sekolah, Tarutung">
+        <meta name="author" content="SMP Negeri 1 Tarutung">
+
         <link rel="icon" type="image/png" href="{{ asset('storage/logo/favicon.png') }}" sizes="48x48">
 
-        {{-- <title inertia>{{ config('app.name', 'Laravel') }}</title> --}}
+        {{-- <title inertia>{{ config('app.name', 'SPARTA') }}</title> --}}
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -16,7 +21,7 @@
         @viteReactRefresh
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
-        <script src="/js/theme.js"></script>
+        <script src="/js/theme.js" defer></script>
     </head>
     <body class="font-sans antialiased max-h-screen">
         @inertia
