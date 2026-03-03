9ed59e74a4ea7263e46e91a8d3a4f63cdfd9ce07
Radot SGX2 <radot2510@gmail.com>
2026-03-03 19:08:41 +0000

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        
+
+        $middleware->trustProxies(at: '*'); // Percayai semua proxy, termasuk Cloudflare
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
